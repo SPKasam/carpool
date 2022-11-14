9e37bdf0158bc39429822641edb038ddff7b5e82
Sai Kasam <pranithkasam7@gmail.com>
2022-11-14 12:56:11 -0600

    bare bones

---
+++ src/components/Card/Card.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Card.css'
+
+const Card = ({from, to, price, name}) => {
+	return (
+		<div className = "card"> 
+			<article className="br2 ba dark-gray b--black-10 mv4 mw5 center">
+			  <img src="http://placekitten.com/g/600/300" className="db w-100 br2 br--top" alt="Photo of a kitten looking menacing."/>
+			  <div className="pa2 ph3-ns pb3-ns">
+			    <div className="dt w-100 mt1">
+			      <div className="dtc">
+			        <h1 className="f5 f4-ns mv0">From {from} to {to}</h1>
+			      </div>
+			      <div className="dtc tr">
+			        <h2 className="f5 mv0">$20</h2>
+			      </div>
+			    </div>
+			    <p className="f6 lh-copy measure mt2 mid-gray">
+			      Hi, I'm {name}, nice to meet you. I'm a senior at UTD studying computer science...
+			    </p>
+			  </div>
+			</article>
+	    </div>
+	)
+}
+
+export default Card;
--- src/components/FromButton.js
+++ src/components/FromButton.js
@@ -10,7 +10,7 @@ class FromButton extends React.Component{
 			<input
 		        className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
 		        type="submit"
-		        value="Need a ride from UTD somewhere else"
+		        value="Need a ride from UTD to somewhere else"
 		        onClick = {() => this.props.onButtonClick("UTD")}
 		    />
 		</div>
