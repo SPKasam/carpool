7f0316b3068adb84d25853ddaecd6d88e6d8e896
Sai Kasam <pranithkasam7@gmail.com>
2023-01-31 18:47:15 -0600

    added title and blurred background

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Intro.css';
-import Mountains from './mountains.jpeg';
+import Mountains from './blurredmountains.png';
 
 const Intro = () => {
   return (
     <div className="maindiv">
-      <img className = "Mountains" src={Mountains} alt="kitten looking menacing."/>
+      <article className='article'>
+        <img className='image' src={Mountains} alt="background" />
+        <h1 className='header'>Welcome to Pooly,</h1>
+        <h1 className='header2'>the unofficial UTD inter-city carpool platform for students</h1>
+      </article>
     </div>
   );
 }
